144f807f963f4f99eb531756c2b2337e63c76ec5
Amit Gupta <amit_@outlook.in>
2024-02-25 09:57:41 -0600

    FIXED: packing C++ files correctly

--- kusp/__init__.py
+++ kusp/__init__.py
@@ -1,3 +1,4 @@
 from .install_kim_model import install_kim_model
 from .server import Server
 from .socket_serve import KUSPServer
+from .__version__ import __version__
--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import find_packages, setup
+from kusp import __version__
 
 setup(
     name="kusp",
-    version="0.0.1",
+    version=__version__,
     packages=find_packages(),
-    package_data={"kusp": ["KUSPPortableModel/**/*"]},
+    package_data={
+        "kusp": [
+            "KUSPPortableModel/**/*",
+            "KUSPPortableModel/*",
+            "KUSPPortableModel/**/*.cpp",
+            "KUSPPortableModel/**/*.h",
+        ]
+    },
+    include_package_data=True,
     install_requires=[
         "pyyaml",
         "loguru",
     ],
     author="Amit Gupta",
     author_email="gupta839@umn.edu",
-    description="kusp",
+    description="kusp: KIM Utility for Serving Potentials",
+    long_description=open(".pypi_readme.md").read(),
+    long_description_content_type="text/markdown",
 )
